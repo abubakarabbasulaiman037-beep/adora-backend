e3c380fc7d288a101895fae53eb609273274f59a
abubakarabbasulaiman037-beep <abubakarabbasulaiman037@gmail.com>
2026-05-23 11:22:34 +0100

    Fix import path for config settings

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from ..config.config import settings
+from config.config import settings
 
 from sqlalchemy import event
 
